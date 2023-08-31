7b39e8a0ec2166274d42f27d21ec9efb4f5e58aa
33hollie33 <hollybarnhurst@gmail.com>
2023-08-31 23:15:17 +0100

    debugging - fixed it

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -70,15 +70,12 @@ const userController = {
       .catch(err => res.status(400).json(err));
   },
 
-
-
-addFriend({ params }, res) {
+addFriend({params}, res) {
   User.findOneAndUpdate(
       {_id: params.id},
       {$push: {friends: params.friendId}},
       {runValidators: true, new: true}
   )
-  
   .then(dbUserData => {
       if(!dbUserData) {
           res.status(404).json({ message: 'No user with this ID!' });
@@ -86,24 +83,25 @@ addFriend({ params }, res) {
       }
       res.json(dbUserData);
   })
-  .catch(err => res.status(400).json(err))
+  .catch(err => res.status(400).json(err));
 },
+removeFriend({params}, res) {
+User.findOneAndUpdate(
+  { _id: params.id},
+  { $pull: { friends: { friendId: params.friendId } } },
+  { new: true }
+)
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => res.json(err));
 
-  removeFriend({ params }, res); {
-    User.findOneAndUpdate(
-      { _id: params.id },
-      { $pull: { friends: { friendId: params.friendId } } },
-      { new: true }
-    )
-    then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id!' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-      .catch(err => res.json(err));
-  }
+}
 };
 
-module.exports = userController;
+module.exports=userController
+
